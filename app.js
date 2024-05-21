e7fd11be255dcffca6c302232777cf0a1967f4cc
Furkan Umut Ç. <umutcomalak@gmail.com>
2024-05-21 20:18:33 +0300

    commit
    commit

--- app.js
+++ app.js
@@ -17,7 +17,24 @@
 // console.log(capitalizeWords("hello everyone"));
 // console.log(makeMoney(100));
 
-const Person = require("./Person");
+// const Person = require("./Person");
 
-const person1 = new Person("John", 30);
-person1.greet();
+// const person1 = new Person("John", 30);
+// person1.greet();
+
+const axios = require("axios");
+
+async function getPost() {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+  // in axios it is different from fetch api, data is already in 'res', so no need to add one more promise
+  console.log(res.data);
+}
+
+getPost();
+
+// Note:
+// npm i -g axios // you can use it globally
+// npm i -D nodemon // package that constantly watches application, it is development dependency
+
+// npm start
+// 'npm run dev' for nodenmon, ctrl+c to stop watching
